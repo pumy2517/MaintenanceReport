a9890cc1b5da81ecc5cec70f322290c463e3895b
pumy2517 <60070067@kmitl.ac.th>
2020-04-18 16:42:47 +0700

    SERVER

--- MaintenanceReport/MaintenanceReport/settings.py
+++ MaintenanceReport/MaintenanceReport/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q!rq)dwm^xc1-q5roa#*lcygg%8vgpmg%ao6rr*f=$n*t8b5t)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ec2-18-213-151-239.compute-1.amazonaws.com']
 
 
 # Application definition
